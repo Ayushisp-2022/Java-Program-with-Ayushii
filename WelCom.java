ce5da5086dad97eb032f075a105fe3a6d20506fe
Ayushi Sanjiv Patil <ayupatil070@gmail.com>
2025-03-28 22:42:07 +0530

    Add files via upload

---
+++ WelCom.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class WelCom
+{
+public static void main(String args[])
+{
+    int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a Number");
+a=sc.nextInt();
+if (a>0)
+{
+System.out.println("Welcome to my world !!");
+}
+}
+}
