8f4ca8749c92cfbdcbf13c7fae652d9005c8c6db
Ayushi Sanjiv Patil <ayupatil070@gmail.com>
2025-03-28 22:40:58 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,24 @@
+// WAP to Show use of any Four Data-types
+ 
+   import java.util.*;                        
+   class  DataType
+   {
+   public static void main (String args [ ])
+   {
+             int  i =10 ;                       //integer data-type used for numeric values
+             float f = 6.4f ;                //floating numbers
+             char c = 'A' ;                  //creating character
+             boolean  b = true  ;        //this data-type only give true or false value
+             short s =5550 ;             //num is larger than short range
+             double d= 5.8 ;            //this data-type store decimal no with high deg precision value as comapre to float
+             long l = 2323l ;         //this data-type need to hold big range of data numbers
+              System.out.println("char :" +i ) ;
+             System.out.println("char :" +f) ;
+             System.out.println("char :" +c ) ;
+            System.out.println("char :" +s) ;
+             System.out.println("char :" +d ) ;
+             System.out.println("char :" +l) ;
+               }
+              }
+
+  
