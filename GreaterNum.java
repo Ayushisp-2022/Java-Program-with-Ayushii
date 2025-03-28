de6c769807983e15a83d56d491b6846d304e7c22
Ayushi Sanjiv Patil <ayupatil070@gmail.com>
2025-03-28 18:00:46 +0530

    Add files via upload
    Program to choose greater number among three using if-else if statement.

---
+++ GreaterNum.java
@@ -0,0 +1,28 @@
+ //WAP to find greatest among three.
+
+   import java.util.*;                                            //importing scanner class for take input from user
+  class GreNum
+    {
+  public static void main ( String args [])
+   {
+ int a, b, c ;  
+ Scanner sc = new Scanner(System.in);                  // scanner object is create for user input
+ System.out.println("Enter the Numbers:");
+  a=sc.nextInt();                                                     //read int as input 
+  b=sc.nextInt();                                                     //read int as input 
+   c=sc.nextInt();                                                   //read int as input 
+     if (a >b && a>c)                                                //code block executes if 1- condition is true
+ System.out.println (" It's nice he can vote ! " +age) ;
+      {
+  System.out.println("The a is  Greater Number is ");
+      }
+    elseif ( b>a && b>c)                                                    //code block executes if 2- condition is true
+      {
+    System.out.println("The   b is  Greater Number is ");
+      }
+  else                                                                           //code block executes if  all condition are false
+    {
+   System.out.println("The c is  Greater Number is ");  
+     }
+     }
+     } 
