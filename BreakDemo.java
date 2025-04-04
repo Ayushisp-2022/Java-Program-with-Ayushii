b62768c1b0f8ddf89cfbdf28249c71dca52451c5
Ayushi Sanjiv Patil <ayupatil070@gmail.com>
2025-04-04 09:54:30 +0530

    Add files via upload
    Real time program using break statement

---
+++ BreakDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class BreakDemo
+{
+	public static void main (String args [])
+	{
+		int i;
+		for(i=1 ;i<=10 ;i++)
+		{
+		if(i==5)
+		{
+			break;
+		}			
+		System.out.println("You are present on Day"+i);
+		}
+	}
+}
